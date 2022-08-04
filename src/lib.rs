88bee59b55abbbfc04c2e07d89496a559a3363e3
Shuyi Cheng <c15271843451@outlook.com>
2022-08-04 20:00:23 +0800

    add bump_memlock_rlimit function
    Signed-off-by: Shuyi Cheng <c15271843451@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,19 @@ pub mod net;
 pub use {
     self::kernel_version::KernelVersion,
 };
+
+
+use anyhow::{bail, Result};
+
+pub fn bump_memlock_rlimit() -> Result<()> {
+    let rlimit = libc::rlimit {
+        rlim_cur: 128 << 20,
+        rlim_max: 128 << 20,
+    };
+
+    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
+        bail!("Failed to increase rlimit");
+    }
+
+    Ok(())
+}
